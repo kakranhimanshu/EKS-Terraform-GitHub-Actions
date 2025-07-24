137c27f2c03e9fcc94997942bc86a2f03fa58c38
kakranhimanshu <142756491+kakranhimanshu@users.noreply.github.com>
2025-07-24 06:07:14 +0000

    c6

--- eks/main.tf
+++ eks/main.tf
@@ -1,49 +1,42 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.1"
+  version = "5.1.0"
 
-  name                 = "${var.env}-vpc"
-  cidr                 = var.vpc_cidr_block
-  azs                  = var.availability_zones
-  public_subnets       = var.public_subnet_cidrs
-  private_subnets      = var.private_subnet_cidrs
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
   enable_dns_hostnames = true
   enable_dns_support   = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 
-  tags = {
-    Environment = var.env
-  }
+  tags = var.tags
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5" # latest compatible version
+  version = "20.8.5"
 
   cluster_name    = var.cluster_name
-  cluster_version = var.eks_version
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
-  subnet_ids     = module.vpc.private_subnets
-  vpc_id         = module.vpc.vpc_id
+  cluster_version = var.cluster_version
+
+  subnet_ids         = module.vpc.private_subnets
+  vpc_id             = module.vpc.vpc_id
+  enable_irsa        = true
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = var.desired_capacity
-      min_size     = var.min_size
-      max_size     = var.max_size
-
-      instance_types = var.instance_types
-      capacity_type  = "ON_DEMAND"
+      instance_types = ["t3.medium"]
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
     }
   }
 
-  tags = {
-    Environment = var.env
-  }
+  tags = var.tags
 }
